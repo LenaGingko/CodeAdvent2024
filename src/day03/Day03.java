1163047a45b0c2bdda140885071317b297bdf587
LenaGingko <lena.arya36@gmail.com>
2024-12-06 11:06:28 +0100

    finish Day 3 part 1

--- src/day03/Day03.java
+++ src/day03/Day03.java
@@ -2,37 +2,33 @@ package day03;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
+import java.util.regex.*;
+import java.util.stream.*;
 
 public class Day03 {
 
     static List<Integer[]> readFile(){
         try {
             File file = new File("Daten/Werte3.txt");
-
             Scanner scanner = new Scanner(file);
 
-            List<Integer[]> reports = new ArrayList<>();
+            List<Integer[]> muls = new ArrayList<>();
+            String regex = "mul\\((\\d{1,3}),(\\d{1,3})\\)";//https://data.templateroller.com/pdf_docs_html/2638/26387/2638707/page_1_thumb_950.png
+            Pattern pattern = Pattern.compile(regex); //https://www.geeksforgeeks.org/matcher-pattern-method-in-java-with-examples/
 
             while (scanner.hasNextLine()) {
                 String line = scanner.nextLine();
-                String[] parts = line.trim().split("\\s+");
+                Matcher matcher = pattern.matcher(line);
 
-                //System.out.println("Length report: " + parts.length); //wie viel Level
-                //Arrays.stream(parts).forEach(System.out::println); //Array []
-                if (parts.length != 0) {
-                    Integer[] integers = Arrays.stream(parts)
-                            .map(Integer::parseInt) // Convert each string to an integer
-                            .toArray(Integer[]::new); // Collect as an Integer array
-                    reports.add(integers);
+                while (matcher.find()) {
+                    int x = Integer.parseInt(matcher.group(1));
+                    int y = Integer.parseInt(matcher.group(2));
+                    muls.add(new Integer[]{x, y});
                 }
             }
             scanner.close();
-
-            return reports;
+            return muls;
         } catch (FileNotFoundException e) {
             System.out.println("File not found");
             e.printStackTrace();
@@ -40,6 +36,12 @@ public class Day03 {
         }
     }
     public static void main(String[] args) {
-        List<Integer[]> reports = readFile();
+        List<Integer[]> mulList = readFile();
+
+        int sum = 0;
+        for ( Integer[] mul : mulList ) {
+            sum += mul[0] * mul[1];
+        }
+        System.out.println("Sum: " + sum);
     }
 }
