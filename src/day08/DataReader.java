5bf74f758095ae0129c274803991f753a220f1d4
LenaGingko <lena.arya36@gmail.com>
2024-12-18 15:11:22 +0100

    Setup Day 9

---
+++ src/day08/DataReader.java
@@ -0,0 +1,31 @@
+package day08;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class DataReader {
+
+    private DataReader() {}
+
+    static String readFile() {
+        try {
+            File file = new File("Daten/Werte9.txt");
+            Scanner scanner = new Scanner(file);
+
+            String line = null;
+            while (scanner.hasNextLine()) {
+                line = scanner.nextLine().trim();
+            }
+            scanner.close();
+
+            return line;
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found");
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
---
+++ src/day08/Day09.java
@@ -0,0 +1,16 @@
+package day08;
+
+public class Day09 {
+
+    public static void main(String[] args) {
+        final String diskMap = DataReader.readFile();
+
+        int id = 0;
+        long checksum = 0;
+
+        //for each file
+        //compress() //recursive?
+
+        System.out.println("Disk Map: " + diskMap);
+    }
+}
